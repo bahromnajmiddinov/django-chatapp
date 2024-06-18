8521f58b2cebe9a3e6c7b5bd65ece94c077c9f48
Bahrom <najmiddinovbahrom402@gmail.com>
2024-06-18 05:57:18 +0500

    'test'

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ else:
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'nima-gap.up.railway.app']
 
-CSRF_TRUSTED_ORGINS = ['https://nima-gap.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://nima-gap.up.railway.app']
 
 INTERNAL_IPS = [
     '127.0.0.1',
