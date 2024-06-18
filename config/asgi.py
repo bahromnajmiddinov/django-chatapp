3095985d4bd6d9f9f5816a60340cec742f0ff8c3
Bahrom <najmiddinovbahrom402@gmail.com>
2024-06-18 05:37:02 +0500

    'updated'

--- config/asgi.py
+++ config/asgi.py
@@ -13,8 +13,10 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat import routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
