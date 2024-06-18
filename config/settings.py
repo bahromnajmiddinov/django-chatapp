70537b2096d1266acf447b9e534c3b2b62993841
Bahrom <najmiddinovbahrom402@gmail.com>
2024-06-18 09:41:35 +0500

    email sending added

--- config/settings.py
+++ config/settings.py
@@ -204,8 +204,14 @@ else:
     }
 
 # EMAIL SERVER CONFIGURATION
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = env('EMAIL_PORT')
-EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = env('EMAIL_HOST')
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = env('EMAIL_PORT')
+    EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+    DEFAULT_FROM_EMAIL = 'NimaGap'
+    ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
