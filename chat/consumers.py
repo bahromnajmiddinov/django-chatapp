7afbd8124535e380de39fab824bcdb87fa4164f8
Bahrom <najmiddinovbahrom402@gmail.com>
2024-06-19 08:03:12 +0500

    bugs fixed

--- chat/consumers.py
+++ chat/consumers.py
@@ -58,7 +58,7 @@ class ChatConsumer(WebsocketConsumer):
             async_to_sync(self.channel_layer.group_discard)(self.chat_username, self.channel_name)
     
     def receiver(self, data):
-        message_object = SingleChatMessage.objects.get(data['message_id'])
+        message_object = SingleChatMessage.objects.get(pk=data['message_id'])
         message = render_to_string('chat/snippets/single-message.html', context={ 'message': message_object, 'maybe_owner': self.user })
         
         data['message'] = message
